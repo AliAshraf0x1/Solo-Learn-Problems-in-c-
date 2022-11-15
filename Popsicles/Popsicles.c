777a9e8d2e197af049e701821bf51b5df2f6b589
Ali Ashraf <95376706+AliAshraf0x1@users.noreply.github.com>
2022-11-15 13:06:17 +0200

    Add files via upload

---
+++ Popsicles/Popsicles.c
@@ -0,0 +1,14 @@
+/***************************by Ali Ashraf*******************************/
+#include<stdio.h>
+int main()
+{
+int siblings, popsicles;
+scanf("%d", &siblings);
+scanf("%d", &popsicles);
+
+if (popsicles % siblings ==0)
+{printf("give away");}
+
+else{printf("eat them yourself");}
+    return 0;
+}
